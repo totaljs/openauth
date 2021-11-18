78d7001b21bb8eadb7a806105dff4067f1d91653
Peter Širka <petersirka@gmail.com>
2021-11-18 15:14:26 +0100

    Updated login.

--- schemas/login.js
+++ schemas/login.js
@@ -5,10 +5,38 @@ NEWSCHEMA('Login', function(schema) {
 	schema.define('url', String);              // optional, redirect URL
 
 	schema.addWorkflow('exec', function($, model) {
+
+
+		var service = MAIN.oauth[model.serviceid];
+		if (!service) {
+			$.invalid('@(Service not found)');
+			return;
+		}
+
+		if (service) {
+
+			var is = true;
+
+			if (service.configuration) {
+				for (var item of service.configuration) {
+					if (item.required && !service.config[item.name]) {
+						is = false;
+						break;
+					}
+				}
+			}
+
+			if (!is) {
+				$.invalid('@(Service is not configured)');
+				return;
+			}
+		}
+
+
 		var id = Date.now().toString(36) + GUID(10);
 		model.id = id;
 		model.token = $.user.token;
-		model.expire = NOW.add('5 minutes');
+		model.expire = NOW.add('15 minutes');
 		model.redirecturl = $.controller.hostname('/oauth/' + model.serviceid + '/');
 		MAIN.sessions[id] = model;
 		$.callback($.controller.hostname('/login/' + id + '/'));
